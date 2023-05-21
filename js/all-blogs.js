ed8b0c35e7358e7a274513b0054ab1a0d374827e
KityLinn <87649871+KityLinn@users.noreply.github.com>
2023-05-21 18:27:38 +0200

    sunday update

--- js/all-blogs.js
+++ js/all-blogs.js
@@ -1,2 +1,25 @@
-import {makeRequest,main_url} from "./export.js";
+import {makeRequest, pageUrl} from "./export.js";
 
+var currentPage = 1;
+//først kan vi lage den som henter neste i listen
+function getMorePosts() {
+  makeRequest(pageUrl(currentPage++), renderList); //denne gir neste sidetall til url function i andre filen
+};
+
+function renderList(blogArray) {
+  //for all posts in blogArray, insert the html into the page at the correct place
+  var listElement = document.querySelector('.blogs-container')//finn hvor det skal settes inn
+  blogArray.forEach(post => {
+    //her må du sette verdier inn i htmlen for hver post 
+    //og så append til listElement
+  });
+  ` <div class="blog-post">
+      <a href="./blog-page.html?id=${blogArray.id}" class="blog-post-link">
+          <h3>${blogArray.title.rendered}</h2>
+          <div class="blog-image-container">
+              <img src="${blogArray._embedded["wp:featuredmedia"][0].source_url}" alt="${blogArray.title.rendered}">>
+          </div>
+          <p>${blogArray.excerpt.rendered}</p>
+      </a>
+    </div>`
+};
--- js/export.js
+++ js/export.js
@@ -1,26 +1,17 @@
 export const main_url = "https://www.linn-eksamen.com/wp-json/wp/v2/posts?_embed&per_page=12";
 export const carousel_url = "https://www.linn-eksamen.com/wp-json/wp/v2/posts?_embed&per_page=8";
-//const fetchbyId = `https://www.linn-eksamen.com/wp-json/wp/v2/posts/${id}?_embed`;
 
-function fetchById(id) {
+export function fetchById(id) { //fetches by ID
     return `https://www.linn-eksamen.com/wp-json/wp/v2/posts/${id}?_embed;`
-   }
+   };
+
+   export function pageUrl(page) { //denne returnerer url med rett nummer
+    return `https://www.linn-eksamen.com/wp-json/wp/v2/posts?_embed&per_page=10&page=${page}`; 
+  };
+
 export const errorMsg = "There was an error ";
 
 
-
-/*
- async function wpData() {
-    const response = await fetch("https://www.linn-eksamen.com/wp-json/wp/v2/posts?_embed");
-    const Data = await response.json();
-    console.log(Data);
-  }
-
- wpData()
- 
- export const data = wpData();
-
-*/
 export async function makeRequest(url, renderFunction) {
     try {
         let res = await fetch(url);
