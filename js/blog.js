ccc3ffc78e527b3505e5dcb5f76634069dd8ff14
KityLinn <87649871+KityLinn@users.noreply.github.com>
2023-05-21 23:21:59 +0200

    images removed

--- js/blog.js
+++ js/blog.js
@@ -3,17 +3,23 @@ import {makeRequest, fetchById} from "./export.js";
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("id");
-makeRequest(fetchById(id),renderSingle);
 
 
 const post = document.querySelector(".post-section");
 
-function renderSingle (singePost) {
+function renderSingle (singlePost) {
+    console.log(singlePost)
     const postContainer = document.createElement("div");
-    postContainer.classList.add("post-containe");
+    postContainer.classList.add("post-container");
     postContainer.innerHTML =
-    `<h2>${singePost.title.rendered}</h2>
-    <img src="${singePost._embedded["wp:featuredmedia"][0].source_url}" alt="${singePost.title.rendered}">
-    <p>${singePost.content.rendered}</p>`
+    `<h2>${singlePost.title.rendered}</h2>
+    <div class="post-image">
+         <img src="${singlePost._embedded["wp:featuredmedia"][0].source_url}" alt="${singlePost.title.rendered}">
+    </div>
+    <div class="text-box">
+         <p>${singlePost.content.rendered}</p>
+    </div>`
     post.appendChild(postContainer)
 };
+
+makeRequest(fetchById(id),renderSingle);
