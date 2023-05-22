26207a4781eebad1040ab5c2ca328f14f005bb81
KityLinn <87649871+KityLinn@users.noreply.github.com>
2023-05-22 14:45:14 +0200

    script

--- js/main.js
+++ js/main.js
@@ -1,34 +1,5 @@
 import {makeRequest, carousel_url} from "./export.js";
 
-
-// Hamburger menu
- 
-const mobNav = document.querySelector(".mobile-burger");
-const mobItems = document.querySelectorAll(".mob-item");
-const mobButton= document.querySelector(".mob-button");
-const closeIcon= document.querySelector(".fa-xmark");
-const openIcon = document.querySelector(".fa-bars");
-
-function toggleMenu() {
-  if (mobNav.classList.contains("showMenu")) {
-    mobNav.classList.remove("showMenu");
-    closeIcon.style.display = "none";
-    openIcon.style.display = "block";
-  } else {
-    mobNav.classList.add("showMenu");
-    closeIcon.style.display = "block";
-    openIcon.style.display = "none";
-  }
-}
-
-mobButton.addEventListener("click", toggleMenu);
-
-mobItems.forEach( 
-  function(mobItems) { 
-    mobItems.addEventListener("click", toggleMenu);
-  }
-)
-
 // Carousel
 
 const next = document.querySelector('#next');
